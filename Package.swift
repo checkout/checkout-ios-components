5cd237c8fc604b1d653965fcb97eb81f33e2c211
Ali Farhadi <ali.farhadi@checkout.com>
2026-05-07 17:22:41 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version: 5.10
 import PackageDescription
 
-let releaseVersion = "1.9.0-rc"
+let releaseVersion = "1.9.0"
 let githubRepo = "checkout/checkout-ios-components"
 
 let sdkChecksum = "5d97ba492e40a54d702dc66433eab72a829bf660d64e22c129d3bed96f5dad4d"
