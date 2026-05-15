d5262f4f12ca8abf3f6b8a87b924be425364ead4
Ali Farhadi <ali.farhadi@checkout.com>
2026-05-15 17:44:57 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
 // swift-tools-version: 5.10
 import PackageDescription
 
-let releaseVersion = "1.9.0"
+let releaseVersion = "1.10.0-rc"
 let githubRepo = "checkout/checkout-ios-components"
 
-let sdkChecksum = "5d97ba492e40a54d702dc66433eab72a829bf660d64e22c129d3bed96f5dad4d"
+let sdkChecksum = "d9b4f08daa7f1d75032675a6e21b1c899d72f5e0bcc1fa2a25ec2f609039cbf7"
 
 let sdkURL = "https://github.com/\(githubRepo)/releases/download/\(releaseVersion)/CheckoutComponentsSDK.xcframework.zip"
 
@@ -39,8 +39,8 @@ let package = Package(
     ),
     .binaryTarget(
       name: "CheckoutComponentsSDK",
-      url: "https://github.com/checkout/checkout-ios-components/releases/download/1.10.0/CheckoutComponentsSDK.xcframework.zip",
-      checksum: "ed3af19e84618840a0361fea186e921f8c216b6369c7a39628eb33a02ee45fa6"
-    ),
+      url: sdkURL,
+      checksum: sdkChecksum
+    )
   ]
 )
