280810c14a5de6bf82b918c2b76e36421f9e3d5a
Ali Farhadi <ali.farhadi@checkout.com>
2026-07-29 16:37:20 +0100

    Release version 1.9.0

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let releaseVersion = "1.9.0"
 let githubRepo = "checkout/checkout-ios-components"
 
-let sdkChecksum = "5d97ba492e40a54d702dc66433eab72a829bf660d64e22c129d3bed96f5dad4d"
+let sdkChecksum = "d645cfa5c2faeb8a0255e5cbc2e1234280d99ed2681438f9abe849d814150028"
 
 let sdkURL = "https://github.com/\(githubRepo)/releases/download/\(releaseVersion)/CheckoutComponentsSDK.xcframework.zip"
 
