d551bc1ae56e7feb972ac456138311b34c34555c
Ali Farhadi <ali.farhadi@checkout.com>
2026-07-29 14:48:42 +0100

    Release version 2.0.0

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ import PackageDescription
 let releaseVersion = "2.2.0"
 let githubRepo = "checkout/checkout-ios-components"
 
-let sdkChecksum = "8b92819b2a7b63ad0d7c880b681ab4ad36824a63913b04feda3525f88fdf5428"
-let paymentMethodsChecksum = "8434b2b35f29b59a7f4d991c7579d5ea4bde1e98da23ce8c79ae3fc925abf9a8"
+let sdkChecksum = "62eaa8421807c4f5c8f22867f50cbf432bdf1bd576c9c6b68a0d7adad7b56e11"
+let paymentMethodsChecksum = "f1511dc047dd94ebc9d99acc734545d8d25ab2a58ea2372fc9af91467a25917c"
 
 let sdkURL = "https://github.com/\(githubRepo)/releases/download/\(releaseVersion)/CheckoutComponentsSDK.xcframework.zip"
 let paymentMethodsURL = "https://github.com/\(githubRepo)/releases/download/\(releaseVersion)/CheckoutPaymentMethods.xcframework.zip"
