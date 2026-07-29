e00e8a051d1f96627bbb3b814ccfe4eb5508ea2f
Ali Farhadi <ali.farhadi@checkout.com>
2026-07-29 17:56:15 +0100

    Release version 1.5.0

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ import PackageDescription
 let releaseVersion = "1.5.0"
 let githubRepo = "checkout/checkout-ios-components"
 
-let sdkChecksum = "042545fcdf3c4a05edddb8a83ccfbfdad2d9b8bc5c339471456cc8331b5057ed"
-let kmpChecksum = "24b2a4abc35d8610b6a4ffbf973fdf491057260b0670c8dadc7a72dc96a1f86d"
+let sdkChecksum = "fe2780d687a527fca13065fd40bd9cb77ad3eab85cd0f9205ef8202eb6087285"
+let kmpChecksum = "9a12d1216105726b4ba40e59e09554438a9406c51e62c93383425aa65e58e99a"
 
 let sdkURL = "https://github.com/\(githubRepo)/releases/download/\(releaseVersion)/CheckoutComponentsSDK.xcframework.zip"
 let kmpURL = "https://github.com/\(githubRepo)/releases/download/\(releaseVersion)/CheckoutKMPRememberMe.xcframework.zip"
