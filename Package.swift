ddf0f331d84ed3db15609d971e227343bbbb8773
Tinashe Makuti <tinashe.makuti@checkout.com>
2026-07-30 09:48:37 +0200

    CHORE: Update Checksums

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ import PackageDescription
 let releaseVersion = "2.0.0"
 let githubRepo = "checkout/checkout-ios-components"
 
-let sdkChecksum = "4a5f41fa4f516e58fd25b67488a576d2e4544fea68f8eb0b228e6dac3386ed8a"
-let paymentMethodsChecksum = "9edfb84e39cb35f80c5e81ce6030cc4dce62ca0f3c36a970b89ce52a2b33035d"
+let sdkChecksum = "83e60a3074196e022376a8a28e2f67157975bb073948171c1242a20a23c25ca4"
+let paymentMethodsChecksum = "4140b0e7d940f3ae2e5863a8902d3ba5f4f7d6ea0a59dfd21530db4209324dc0"
 
 let sdkURL = "https://github.com/\(githubRepo)/releases/download/\(releaseVersion)/CheckoutComponentsSDK.xcframework.zip"
 let paymentMethodsURL = "https://github.com/\(githubRepo)/releases/download/\(releaseVersion)/CheckoutPaymentMethods.xcframework.zip"
