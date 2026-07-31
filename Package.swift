5a77a669cdc69a6aa5857473ed2c145e14da7e74
Ali Farhadi <ali.farhadi@checkout.com>
2026-07-31 16:43:24 +0100

    Update Package.swift file

--- Package.swift
+++ Package.swift
@@ -1,14 +1,16 @@
 // swift-tools-version: 5.10
 import PackageDescription
 
-let releaseVersion = "2.4.0"
+let releaseVersion = "2.4.0-rc"
 let githubRepo = "checkout/checkout-ios-components"
 
 let sdkChecksum = "8a86c9e4ec2d05964c3793b802dadb03cf44b4ac5622b0123076064f664fc649"
 let paymentMethodsChecksum = "354e719473656541d1167ee16d0523205bfe138e846b9f743b75e86302157155"
+let klarnaChecksum = "980a96094ebe27b0724d6ad8b7dc45559bb58ba09df3e29290de006e7f1f92f6"
 
 let sdkURL = "https://github.com/\(githubRepo)/releases/download/\(releaseVersion)/CheckoutComponentsSDK.xcframework.zip"
 let paymentMethodsURL = "https://github.com/\(githubRepo)/releases/download/\(releaseVersion)/CheckoutPaymentMethods.xcframework.zip"
+let klarnaURL = "https://github.com/\(githubRepo)/releases/download/\(releaseVersion)/CheckoutKlarna.xcframework.zip"
 
 let package = Package(
   name: "CheckoutComponents",
@@ -58,6 +60,11 @@ let package = Package(
       name: "CheckoutPaymentMethods",
       url: paymentMethodsURL,
       checksum: paymentMethodsChecksum
+    ),
+    .binaryTarget(
+      name: "CheckoutKlarna",
+      url: klarnaURL,
+      checksum: klarnaChecksum
     ),
   ]
 )
