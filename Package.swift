c660f8dfa473c9c4f787f2e695abaa4b39d4ebbe
Ali Farhadi <ali.farhadi@checkout.com>
2026-07-31 16:48:09 +0100

    Update checksums

--- Package.swift
+++ Package.swift
@@ -4,9 +4,9 @@ import PackageDescription
 let releaseVersion = "2.4.0-rc"
 let githubRepo = "checkout/checkout-ios-components"
 
-let sdkChecksum = "8a86c9e4ec2d05964c3793b802dadb03cf44b4ac5622b0123076064f664fc649"
-let paymentMethodsChecksum = "354e719473656541d1167ee16d0523205bfe138e846b9f743b75e86302157155"
-let klarnaChecksum = "980a96094ebe27b0724d6ad8b7dc45559bb58ba09df3e29290de006e7f1f92f6"
+let sdkChecksum = "c7975d437a2a21ed61f4e56c09e87eb574c71bddcf65618c55db8c79e560b5f6"
+let paymentMethodsChecksum = "8848e0cd9f768195d1eecc0e8abc53cdfee84546b37e67375cf3fd4c966f8ef9"
+let klarnaChecksum = "a43d3651e473c111c90a3b0b39b2f8fd45dfbe5510eb83c10912c0925dc8ffc0"
 
 let sdkURL = "https://github.com/\(githubRepo)/releases/download/\(releaseVersion)/CheckoutComponentsSDK.xcframework.zip"
 let paymentMethodsURL = "https://github.com/\(githubRepo)/releases/download/\(releaseVersion)/CheckoutPaymentMethods.xcframework.zip"
